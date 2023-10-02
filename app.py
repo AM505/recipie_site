01bf6048f2616116494a47824293d7a8fd10e48c
William Morgan <williammorgan864@gmail.com>
2023-10-02 22:01:19 +0100

    feat: added boilerplate for linking arbitrary paths
    added boilerplate so can have a path to any valid recipe

--- app.py
+++ app.py
@@ -12,6 +12,17 @@ def index():
         data = json.load(json_data)
     return render_template("index.html", recipes=data)
 
+@app.route("/<recipe_name>")
+def recipe(recipe_name):
+    recipe = {}
+    with open("data/recipes.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["recipe_url"] == recipe_name:
+                recipe = obj
+    return "<h1>"+recipe["name"]+"</h1>"
+    
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),

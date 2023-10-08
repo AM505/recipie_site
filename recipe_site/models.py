cc4806ad85464c2eaa710120ae89c2e4d52c48d4
William Morgan <williammorgan864@gmail.com>
2023-10-08 04:54:37 +0100

    added user login and registration

--- recipe_site/models.py
+++ recipe_site/models.py
@@ -1,4 +1,5 @@
 from recipe_site import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,8 +16,15 @@ class Recipe(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(355))
     is_admin = db.Column(db.Boolean)
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
     def __repr__(self):
--- recipe_site/routes.py
+++ recipe_site/routes.py
@@ -1,13 +1,13 @@
 from flask import ( render_template, 
-    redirect, request, session, url_for)
+    redirect, request, session, url_for, flash)
 from recipe_site import app,  db
 from recipe_site.models import User, Recipe
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index")#, defaults={'username': None}
 def index():
-    categories = list(Recipe.query.order_by(Recipe.recipe_name).all())
-    return render_template("base.html")
+    recipes = list(Recipe.query.order_by(Recipe.recipe_name).all())
+    return render_template("base.html", recipes=recipes)
 
 @app.route("/admin_recipe", methods=["GET", "POST"])
 def admin_recipie():
@@ -21,11 +21,42 @@ def add_recipe():
     return render_template("admin_recipe.html")
 
 ## lets do user login 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        existing_user = User.query.filter_by(username=request.form.get("username")).first()   
+    
+        if existing_user:
+            if existing_user.check_password(request.form.get("password")):
+                session["user"] = request.form.get("username")
+                session["is_admin"] = request.form.get("is_admin")
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("index", username=session["user"]))
+
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
     return render_template('login.html')
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template('register.html')
+    if request.method == "POST":
+        #check if user exists in db
+
+        existing_user = User.query.filter_by(username=request.form.get("username")).first()        
+        if existing_user:
+            flash ("Username already exists")
+            return redirect(url_for("login"))
+        
+        user = User(username=request.form.get("username"), is_admin=False)
+        user.set_password(request.form.get("password"))
+        db.session.add(user)
+        db.session.commit()
+        session["user"] = request.form.get("username")
+        flash("Registration successful")
+        return redirect(url_for("index", username=session["user"]))
+    return render_template("register.html")
 
